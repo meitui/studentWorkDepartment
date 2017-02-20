d3e7359aba29e41117f843b8a4b1365f066c66dd
meitui <2372466207@qq.com>
2017-02-20 09:19:58 +0800

    第一次优化

--- js/idea/base.js
+++ js/idea/base.js
@@ -1,35 +1,29 @@
 function displaySubMenu(li) {
-var subMenu = li.getElementsByTagName("ul")[0];
-subMenu.style.display = "block";
-
+    var subMenu = li.getElementsByTagName("ul")[0];
+    subMenu.style.display = "block";
 }
+
 function hideSubMenu(li) {
-var subMenu = li.getElementsByTagName("ul")[0];
-subMenu.style.display = "none";
+    var subMenu = li.getElementsByTagName("ul")[0];
+    subMenu.style.display = "none";
 }
-var sdelay=0;
+
+var sdelay = 0;
+
 function returnTop() {
- window.scrollBy(0,-500);
- if(document.body.scrollTop>0) { 
-  sdelay= setTimeout('returnTop()',50);
- }
+    window.scrollBy(0, -500);
+    if (document.body.scrollTop > 0) {
+        sdelay = setTimeout('returnTop()', 50);
+    }
 }
 
-// $('body').on('click', '.js-open-nav', function () {
-
-//     var t = $('nav');
-//     t.hide();
-//     $('.search-wrap').hide();
-//     if ($('.user-wrap').css('display') == 'block') {
-//         $('.htmlBox,body').removeAttr('style');
-//         t.stop().animate({'opacity': '0'}, 200, function () {
-//             t.hide();
-//         });
-//         $('.user-wrap').hide();
-//     } else {
-//         $('.htmlBox,body').css({'overflow': 'hidden','height':document.documentElement.clientHeight});
-//         t.show().animate({'opacity': '1'}, 300);
-//         $('.user-wrap').show()
-//     }
-//     $('.navbar-toggle').toggleClass('active');
-// });
+$('.js-open-nav').on('click', function() {
+	if ($('.user-wrap').css('display') == 'block') { 	
+        $('.container,footer').toggle();
+        $('.user-wrap').css('display', 'none');
+    } else {
+         $('.container,footer').toggle();
+        $('.user-wrap').css('display', 'block');
+    }
+    $('.navbar-toggle').toggleClass('active');
+});
